cb8c0d553aa6588188fbda62d618f8c3b6a734e9
Lavanya <40706613+LavanyaElango@users.noreply.github.com>
2019-10-16 21:40:07 -0700

    sorting an array in descending order
    1.get the size of the array from user
    2.get the array elements from user
    3.sort the array and the reverse it to get the values in descending
    4.print the array

---
+++ arrayDesc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace code
+{
+    class arrayDesc
+    {
+        public static void Main()
+        {
+            int n = 0;
+            // 1.get the size of the array from user
+            Console.WriteLine("Enter the size of the array \n");           
+            n = Convert.ToInt32(Console.ReadLine());
+
+            // 2.get the array elements from user
+            int[] arr = new int[n];
+            Console.WriteLine("Enter the values for your array \n");
+            for (int i = 0; i < n; i++)
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+
+            // 3.sort the array and the reverse it to get the values in descending
+            Array.Sort(arr);
+            Array.Reverse(arr);
+
+            // 4.print the array
+            Console.WriteLine("\n your array in descending order \n");
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write("{0}  ", arr[i]);
+
+            Console.ReadKey();
+
+        }
+    }
+}
